9717d1e4285e28173273ff36a48becb08da93c83
Lolot Stefan-Bogdan <lolotstefanbogdan@gmail.com>
2023-02-07 02:06:18 +0200

    login firebase & private route

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,47 @@
+import { async } from '@firebase/util';
 import React from 'react'
+import {Link, useNavigate} from 'react-router-dom';
+import {UserAuth} from '../context/AuthContext'
 
 const Navbar = () => {
+
+  const {user, logOut} = UserAuth()
+  //console.log(user)
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await logOut();
+      navigate('/');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className = 'flex items-center justify-between p-4 z-[100] w-full absolute'>
-        <h1 className = 'text-red-600 text-4xl font-bold cursor-pointer'>Movie Kage</h1>
-        <div>
+      <Link to='/'>
+      <h1 className = 'text-red-600 text-4xl font-bold cursor-pointer'>Movie Kage</h1>
+      </Link>
+        {
+          user?.email ?  (<div>
+          <Link to='/account'>
+            <button className = 'text-white pr-4'>Account</button>
+          </Link>
+            <button onClick = {handleLogout} className = 'bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>Logout</button>
+        </div>) 
+        :
+        (<div>
+          <Link to='/login'>
             <button className = 'text-white pr-4'>Sign in</button>
+          </Link>
+          <Link to='/signup'>
             <button className = 'bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>Sign up</button>
-        </div>
+          </Link>
+        </div>)
+        }
     </div>
-  )
-}
+  );
+};
 
 export default Navbar
