dd8ee1a745c9c89d8b46c14b0e2973d730559312
Lolot Stefan-Bogdan <lolotstefanbogdan@gmail.com>
2023-02-10 17:00:50 +0200

    small refactor

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,17 @@
 import React, { useState , useEffect} from 'react'
 import requests  from '../Request'
 import axios from 'axios'
+import { UserAuth } from '../context/AuthContext';
+import {db} from '../firebase'
+import { arrayUnion, doc, updateDoc } from 'firebase/firestore'; 
+
 
 const Main = () => {
+
+    const [saved, setSaved] = useState(false);
+    const { user } = UserAuth();
+
+    const movieID = doc(db, 'users', `${user?.email}`);
 
     const [movies, setMovies] = useState([])
 
@@ -14,7 +23,7 @@ const Main = () => {
         })
     },[])
 
-    console.log(movie);
+    // console.log(movie);
 
     const truncateString = (str, num) => {
         if (str?.length > num) {
@@ -24,20 +33,39 @@ const Main = () => {
         }
       };
       
+      const saveShow = async () => {
+        if (user?.email) {
+          setSaved(true);
+          await updateDoc(movieID, {
+            savedShows: arrayUnion({
+              id: movie.id,
+              title: movie.title,
+              img: movie.backdrop_path,
+            }),
+          });
+        }else {
+          alert('Please log in to save movies');
+        }
+      };
+
+      const showAlert = () => {
+        alert('Please use Youtube to watch the trailer');
+      };
+
   return (
-    <div className = 'w-full h-[600px] text-white'>
+    <div className = 'w-full h-screen text-white'>
         <div className = "w-full h-full">
-            <div className = 'absolute w-full h-[600px] bg-gradient-to-r from-black'></div>
+            <div className = 'absolute w-full h-screen bg-gradient-to-r from-black'></div>
             <img className = 'w-full h-full object-cover' src = {`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt = {movie?.title} />
             
-            <div className='absolute w-full top-[20%] p-4 md:p-8'>
+            <div className='absolute w-full top-[20%] p-5 md:p-8'>
           <h1 className='text-3xl md:text-5xl font-bold'>{movie?.title}</h1>
           <div className='my-4'>
-            <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5'>
+            <button onClick = {showAlert} className='border bg-gray-300 text-black border-gray-300 py-2 px-5'>
               Play
             </button>
-            <button className='border text-white border-gray-300 py-2 px-5 ml-4'>
-              Watch Later
+            <button onClick = {saveShow} className='border text-white border-gray-300 py-2 px-5 ml-4'>
+              Save Show
             </button>
           </div>
           <p className='text-gray-400 text-sm'>
